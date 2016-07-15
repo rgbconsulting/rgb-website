1a355c06ca55d9f0d9a4aee8f85c0187f84c565e
Gelu <gelu.boros@rgbconsulting.com>
2016-07-15 10:58:09 +0200

    [FIX] Bug fixes and optimizations

--- theme_bootswatch_extra/__openerp__.py
+++ theme_bootswatch_extra/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'RGB Bootswatch Themes',
-    'version': '1.0',
+    'version': '2.0',
     'depends': ['website'],
     'license': 'AGPL-3',
     'author': "RGB Consulting SL",
